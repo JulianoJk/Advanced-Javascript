27a56fef9cc3fdef4129f951ef7a5e839ea1606a
JulianoJk <tzouliano10@gmail.com>
2021-10-27 03:09:30 +0300

    Added restart button & fixed some major issues

--- Assigments/Quiz.js
+++ Assigments/Quiz.js
@@ -1,5 +1,7 @@
 let questions_array =[]; //Empty array to store the questions(question+ options)
 let currentQuestion = 0; //Set the question number
+let correct;
+let incorrect;
 
 //Once the page loads, assign this function to the start button
 function loaded(){
@@ -7,7 +9,9 @@ function loaded(){
     starts_button.addEventListener('click', function(){
         startQuiz();
         //Get a question when the 'start' button is clicked for first time
-        getQuestion() 
+        getQuestion();
+        //display the submit button
+        document.getElementById('submit-div').style.display="block";
     })
 }
 
@@ -44,7 +48,7 @@ function startQuiz(){
 
 //get API from the open trivia api
 function getQuestion(){
-    fetch("https://opentdb.com/api.php?amount=4&difficulty=easy&type=multiple")
+    fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
     .then(res=> data=res.json())
     .then(data => {
         questions_array = data['results']; //Assign everything the 'results' obj
@@ -53,37 +57,58 @@ function getQuestion(){
 }
 
 function displayQuestion(){
-    let display_question ="<div><b>" + questions_array[currentQuestion]['question'] + "</b></div>"; 
 
+    let display_question ="<div><b>" + questions_array[currentQuestion]['question'] + "</b></div>"; 
 
 
     //In order to shuffle the correct answer instead of being always the first or last
     let randomPos = Math.floor(Math.random() * 4);
-    let correct = questions_array[currentQuestion]['correct_answer']; //save the correct answer of the quiz
-    let incorrect = questions_array[currentQuestion]['incorrect_answers']; //save the correct answer of the quiz
+    correct = questions_array[currentQuestion]['correct_answer']; //save the correct answer of the quiz
+    incorrect = questions_array[currentQuestion]['incorrect_answers']; //save the correct answer of the quiz
 
     /*Loop to shuffle the answers. If the i is matched with the randomPos, add the correct answer to that position, else add all the wrong answers. Lastly check if randomPos is 3, if yes add the correct answer in the last position*/
-    for(let i = 0; i < 3; i++) {
+    for( let i =0; i < 3; i++) {
         //Assign correct answer to index i if true
         if(i == randomPos) {
-            display_question+="<div><input type='radio' class=\"radio-buttons\" name=\"question-group\">" + correct + "</div>"
+            display_question+="<div><input type='radio' class=\"radio-buttons\" name=\"question-group\" value='" + correct + "'>" + correct + "</div>"
         }
         //Print the incorrect options if above if not true
-        display_question+="<div><input type='radio' class=\"radio-buttons\" name=\"question-group\">" + incorrect[i] + "</div>"
+        display_question+="<div><input type='radio' class=\"radio-buttons\" name=\"question-group\"value='" + incorrect[i]+ "'>" + incorrect[i] + "</div>"
     }
     //Assign correct answer to index 3 if randPos is 3
     if(randomPos == 3){
-        display_question+="<div><input type='radio' class=\"radio-buttons\" name=\"question-group\">" + correct + "</div>"
+        display_question+="<div><input type='radio' class=\"radio-buttons\" name=\"question-group\" value='" + correct + "'>" + correct + "</div>"
     }  
     document.getElementById('questions').innerHTML=display_question;
-    console.log(correct);
+
+    console.log(correct);//TODO:Delete it
+    // display question order and length (1/10);
+    document.getElementById('current-question').innerHTML= "<div>" + (currentQuestion+1) + '/' +questions_array.length + "</div>"; 
 }
 
     
 // Add a submit button
 function submitAnswer (){
-    let userAnswer = document.querySelector("input[name=question-group]:checked").innerHTML;
-    console.log(userAnswer);
+    let userAnswer = document.querySelector("input[name=question-group]:checked").value;//Take the value from the options
+    if(userAnswer == correct){
+        //score++
+    }
+    //Display next question
+    currentQuestion++;
+    displayQuestion();
+
+    //After the quiz is over, display score, create a button to start a new quiz
+    if(currentQuestion == (questions_array.length -1)){
+        //Hide everything after the quiz ends and display only the score and the restart button
+        document.getElementById('Quiz').style="display:none"
+        document.getElementById('Game-Over').style="display:block";
+    }
+}
+
+
+//restart the quiz after ending
+function restart(){
+    window.location.reload();
 }
 
 //Check score
