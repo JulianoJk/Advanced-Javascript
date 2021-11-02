12904513b4cb7a9eefe821115d8ca2ffae786c02
JulianoJk <tzouliano10@gmail.com>
2021-11-02 02:50:29 +0200

    Added cover and more info

--- Books/Books.js
+++ Books/Books.js
@@ -1,20 +1,3 @@
-/*
-Build a web page books.html.
-
-This page will have a text field and a button. The user will type an ISBN of a book and upon click the page will display the details of the book (e.g. Title, publication year, number of pages, etc) and the Cover page image.
-
-The APIs for book data can be found here: https://openlibrary.org/developers/api
-
-ISBN API: https://openlibrary.org/isbn/9780140328721.json (replace 9780140328721 with the user's input)
-
-Covers API: https://covers.openlibrary.org/b/isbn/0385472579-M.jpg (replace 0385472579 with the user's input)
-*/
-
-//TODO: Check if user's input is only numbers and not letters
-//TODO: Get book's API
-//TODO: Save the Books API to a variable and try to replace the url of api with users var: https://openlibrary.org/isbn/[usersInput].json
-
-
 //Retrieve user's ISBN number 
 function getISBN(){
     let userInput = document.getElementById('text-field');
@@ -31,12 +14,14 @@ function getISBN(){
 }
 function retrieveData(number){
     let url =`https://openlibrary.org/isbn/${number}.json`;
+    let img = `https://covers.openlibrary.org/b/isbn/${number}-M.jpg`
+    
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        displayInfo(data);
+        displayInfo(data, img);
     })
     // Catch error(found no ISBN)
     .catch(err =>{
@@ -54,10 +39,17 @@ function clearMsgs(){
     
 }
 //Display info about the book(Title, publication year, number of pages, etc)
-function displayInfo(data){
-    // Make div visible //TODO:Delete it
-    document.getElementById('book-title').style.display="block";
-    //Display Title
+function displayInfo(data, img){
+    //Display the div containing all book information div
+    document.getElementById('books-info-div').style.display="block";
+    
     document.getElementById('book-title').innerHTML =`Title: ${data['title']}`
-    //TODO: add each element to a new line
+    //Display pages
+    document.getElementById('book-pages').innerHTML=`Pages: ${data['number_of_pages']}`
+    //Display publication year
+    document.getElementById('book-publ-year').innerHTML=`Publication year: ${data['publish_date']}`
+
+    //Display book cover
+    document.getElementById('cover').src = img;
+    
 }
